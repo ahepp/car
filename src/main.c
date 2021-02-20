b46322cb0aa0451e3ded312b734e2eb9e80a8373
Andrew Hepp <andrew.hepp@ahepp.dev>
2021-02-19 23:03:18 -0800

    fixed expression lifetime

--- src/main.c
+++ src/main.c
@@ -10,13 +10,16 @@ int main(int arg, char** argv) {
         fgets(in, CAR_MAX_STR_LEN, stdin);
         in[strcspn(in, "\n")] = 0; // strip newline
 
-        car_expr *e;
+        car_expr *e = NULL;
         car_expr *r;
         car_parse(&e, in);
         car_eval(&r, e);
         if(r)
             fputs(r->tag, stdout);
         fputc('\n', stdout);
+
+        car_expr_free(e);
+        car_expr_free(r);
     }
     return 0;
 }
