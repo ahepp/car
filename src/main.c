cd3980e550a11302da7c4b66c01e320e3deb1817
Andrew Hepp <andrew.hepp@ahepp.dev>
2021-06-29 20:01:42 -0700

    Removed unneeded initialization.

--- src/main.c
+++ src/main.c
@@ -5,12 +5,12 @@
 
 int main(int arg, char** argv) {
     while(1) {
-        fputs("car> ", stdout);
         char in[CAR_MAX_STR_LEN];
+        fputs("car> ", stdout);
         fgets(in, CAR_MAX_STR_LEN, stdin);
         in[strcspn(in, "\n")] = 0; // strip newline
 
-        car_expr *e = NULL;
+        car_expr *e;
         car_expr *r;
         car_parse(&e, in);
         car_eval(&r, e);
